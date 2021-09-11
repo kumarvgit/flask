6199ab61cb359040bbfc621084d7e3bf3b27f1db
kumarvgit <vaibhav.cs.kumar@gmail.com>
2021-09-11 06:44:51 +0100

    adding

--- relationship/data_insert.py
+++ relationship/data_insert.py
@@ -1,39 +1,44 @@
+# This script will create some puppies, owners, and toys!
+# Note, if you run this more than once, you'll be creating dogs with the same
+# name and duplicate owners. The script will still work, but you'll see some
+# warnings. Watch the video for the full explanation.
 from models import db, Puppy, Owner, Toy
 
+# Create 2 puppies
+rufus = Puppy("Rufus")
+fido = Puppy("Fido")
 
-db.create_all()
-
-# Creating puppy
-
-rufus = Puppy('Rufus')
-fido = Puppy('Fido')
-
+# Add puppies to database
 db.session.add_all([rufus, fido])
 db.session.commit()
 
-# Check
+# Check with a query, this prints out all the puppies!
 print(Puppy.query.all())
 
-rufus = Puppy.query.filter_by(name='Rufus').first()
+# Grab Rufus from database
+# Grab all puppies with the name "Rufus", returns a list, so index [0]
+# Alternative is to use .first() instead of .all()[0]
+rufus = Puppy.query.filter_by(name='Rufus').all()[0]
 
-jose = Owner('Jose', rufus.id)
+# Create an owner to Rufus
+jose = Owner("Jose", rufus.id)
 
+# Give some Toys to Rufus
 toy1 = Toy('Chew Toy', rufus.id)
-toy2 = Toy('Ball', rufus.id)
+toy2 = Toy("Ball", rufus.id)
 
+# Commit these changes to the database
 db.session.add_all([jose, toy1, toy2])
 db.session.commit()
 
-# Grab Rufus
-rufus = Puppy.query.filter_by(name='Rufus')
+# Let's now grab rufus again after these additions
+rufus = Puppy.query.filter_by(name='Rufus').first()
 print(rufus)
-rufus.report_toys()
 
+# Show toys
+print(rufus.report_toys())
 
-if __name__ == '__main__':
-    print(f'running from main')
-else:
-    print(f'running as module')
-
-
-
+# You can also delete things from the database:
+# find_pup = Puppy.query.get(1)
+# db.session.delete(find_pup)
+# db.session.commit()
